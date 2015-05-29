c7fe8cec9693215404e48bbf9a8268e4e29ef236
unknown <mintzj@KIDD086-PC02.science.oregonstate.edu>
2015-05-29 10:45:20 -0700

    Update Micro Array Read file

--- microarray_read.R
+++ microarray_read.R
@@ -1,7 +1,11 @@
 #  (Assign the appropriate local directory for your data and brain)
 # Brain Choices: 
-directory <- "Z:/ST599/Project3/brain/brain_images/H0351.2001"
+#directory <- "Z:/ST599/Project3/brain/brain_images/H0351.2001"
+brains_directory <- "Z:/ST599/Project3/brain/brain_images"
+brain_folders <- list.dirs(path = brains_directory, recursive = F)
 
+# Select a brain (1-8)
+current_brain_folder <- brain_folders[5]
 
 # Read in the annotation
-annotation  <- read.csv(paste(directory,"SampleAnnot_1.csv", sep = "/"))
+annotation  <- read.csv(paste(current_brain_folder,"SampleAnnot.csv", sep = "/"))
